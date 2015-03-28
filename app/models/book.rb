215ce88f84d33aa720a11c2d0103b5a181c9c0a4
Kuniaki IGARASHI <igaiga@gmail.com>
2015-03-28 16:37:23 +0900

    Refectering

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,10 @@
 class Book
   def self.informations
-    [
-      {"title": "線形代数入門"},
-      {"title": "数学的に有り得ない"}
-    ]
+    [self.titles]
+  end
+
+  def self.titles
+    [{"title": "線形代数入門"},
+     {"title": "数学的に有り得ない"}]
   end
 end
