07f4a68654c157da8d9da8f9dca8ae848893fd9c
YAMASHITA Hiroki <hirokick6186@gmail.com>
2015-03-28 16:33:09 +0900

    added recommend action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,4 +5,11 @@ class BooksController < ApplicationController
       format.json { render json: {books: Book.informations } }
     end
   end
+
+  def recommend
+    respond_to do |format|
+      format.html { render text: 'html waiwai' }
+      format.json { render json: {books: Book.informations } }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'books/recommend'
+
   get 'books/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
