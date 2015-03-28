0764342c9e2094b69676d75edfd140f9194fc3c4
YAMASHITA Hiroki <hirokick6186@gmail.com>
2015-03-28 16:00:51 +0900

    return book title json

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   def index
     respond_to do |format|
       format.html { render text: 'html waiwai' }
-      format.json { render json: {books: "ここにほんのタイトルがはいります。"} }
+      format.json { render json: {books: {"title": "線形代数入門"}} }
     end
   end
 end
