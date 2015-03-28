8abe67306d56e407dc5abccc40d956c8b9113060
Kuniaki IGARASHI <igaiga@gmail.com>
2015-03-28 15:43:02 +0900

    create json response

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,8 @@
 class BooksController < ApplicationController
   def index
+    respond_to do |format|
+      format.html { render text: 'html waiwai' }
+      format.json { render json: {title: "terakoya"} }
+    end
   end
 end
