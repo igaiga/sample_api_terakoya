523437d7319a084d41aeb59851f19ca2e374553c
Kuniaki IGARASHI <igaiga@gmail.com>
2015-03-28 16:35:01 +0900

    Implement fake method.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
   def recommend
     respond_to do |format|
       format.html { render text: 'html waiwai' }
-      format.json { render json: {books: Book.informations } }
+      format.json { render json: { title: "四月は君の嘘" } }
     end
   end
 end
