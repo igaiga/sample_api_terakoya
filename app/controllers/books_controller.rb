265fcd32562fdc0e343de4eb7867798e982ee7bf
Kuniaki IGARASHI <igaiga@gmail.com>
2015-03-28 16:13:45 +0900

    Refectering books model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   def index
     respond_to do |format|
       format.html { render text: 'html waiwai' }
-      format.json { render json: {books: {"title": "線形代数入門"}} }
+      format.json { render json: {books: Book.informations } }
     end
   end
 end
---
+++ app/models/book.rb
@@ -0,0 +1,5 @@
+class Book
+  def self.informations
+    {"title": "線形代数入門"}
+  end
+end
